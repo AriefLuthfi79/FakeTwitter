7d75b4ac92f4a784b6d5776c716507ee0b2ab7b2
arief <ariefluthfi799@gmail.com>
2018-05-24 11:05:55 +0700

    Add password resets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save { downcase_email }
   before_create :create_activation_digest
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -10,23 +10,19 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-  # Downcase email before saving to database
-  def downcase_email
-    self.email = email.downcase
-  end
-
+  
   # Returns the hash digest of the given string
   def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost) 
   end
-
+  
   # Remember a user in the database for use in session
   def remember
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
   end
-
+  
   # Returns random token
   def self.new_token
     SecureRandom.urlsafe_base64
@@ -38,20 +34,46 @@ class User < ApplicationRecord
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
-
+  
   # Forget the users
   def forget
     update_attribute(:remember_digest, nil)
   end
-
+  
+  # Send activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-
+  
+  # Activate an account
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
   end
+  
+  # Sets the password reset attributes
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
 
+  # Send password reset to email
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  # Return true if a password reset has expired
+  def password_reset_expired?
+    self.reset_sent_at < 2.hours.ago
+  end
+
+  private
+  # Downcase email before saving to database
+  def downcase_email
+    self.email = email.downcase
+  end
+  
+  # Create and assigns the activation token and digest
   def create_activation_digest
     self.activation_token = User.new_token
     self.activation_digest = User.digest(self.activation_token)
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
